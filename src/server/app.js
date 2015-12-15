361ebc72013665c9f25fa92e95103afbc945f537
Blackstock <charlieblackstock@gmail.com>
2015-12-15 13:23:56 -0700

    updated the server side code for app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,74 +1,31 @@
-// *** main dependencies *** //
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var swig = require('swig');
 
-
-// *** routes *** //
-var routes = require('./routes/index.js');
-
-
-// *** express instance *** //
 var app = express();
 
+// *** Models *** //
 
-// *** view engine *** //
-var swig = new swig.Swig();
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
+app.set('port', process.env.PORT || 3000);
 
+var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
+});
 
-// *** static directory *** //
-app.set('views', path.join(__dirname, 'views'));
-
-
-// *** config middleware *** //
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// *** main routes *** //
-app.use('/', routes);
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client', 'index.html'));
 });
 
-
-// *** error handlers *** //
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.get('/controller', function(req, res) {
+    //Serve up phone page here.
+    res.sendFile(path.join(__dirname, '../client', 'controller.html'));
 });
+
 
 
 module.exports = app;
